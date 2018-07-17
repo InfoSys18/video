271db7440c7305e52ae4e76ae91579213ae6d78b
InfoSys18 <infosys@i.ua>
2018-07-17 21:31:56 +0300

    Created default Controller, View and Layout page.

--- config/web.php
+++ config/web.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: InfoSys
- * Date: 17.07.2018
- * Time: 19:23
- */
-
     return [
         'id' => 'video',
         'basePath' => realpath(__DIR__ . '/../')
