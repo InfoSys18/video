6149c9cbd4a4930dd00f1d5ce6342e46e573286f
InfoSys18 <infosys@i.ua>
2018-07-17 21:33:23 +0300

    Created default Controller, View and Layout page.

---
+++ views/layouts/main.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Video School</title>
+    </head>
+    <body>
+        <?php echo $content ?>
+    </body>
+</html>
---
+++ views/site/index.php
@@ -0,0 +1,3 @@
+<h1>Video School</h1>
+
+<h3>Welcome to the Video Paradise!</h3>
